80317b0c4fb99ba661e47916e9dbc76c60e68ad9
JeanDeCarli <jean.carlo.jc1@gmail.com>
2013-10-18 16:33:48 -0300

    implementado a leitura do arquivo

--- RBT/src/rbt/Principal.java
+++ RBT/src/rbt/Principal.java
@@ -4,9 +4,6 @@
  */
 package rbt;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -19,9 +16,49 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        
-        System.out.println("Informe o nome do arquivo com o caminho:\n");
-        
+        int op;
+
+        Scanner scan = new Scanner(System.in);
+        Tree t = new Tree();
+
+        do {
+
+            System.out.println("Escolha uma das opções abaixo:\n"
+                    + "1 - para importar arquivo\n"
+                    + "2 - para adicionar nódo\n"
+                    + "3 - para remover nódo\n"
+                    + "4 - para procurar um nódo\n"
+                    + "5 - para sair");
+            op = scan.nextInt();
+            switch (op) {
+                case 1: {
+                    System.out.println("Digite o nome do arquivo com o caminho:\n");
+                    t.crateTree(scan.next());
+                    break;
+                }
+                case 2: {
+                    System.out.println("Digite um par de chave=valor:\n");
+                    t.addNodo(scan.nextLine());
+                    break;
+                }
+                case 3: {
+                    System.out.println("Digite um par de chave=valor:\n");
+                    t.rmNodo(scan.nextLine());
+                    break;
+                }
+                case 4: {
+                    System.out.println("Digite um par de chave=valor:\n");
+                    t.searchNodo(scan.nextLine());
+                    break;
+                }
+                case 5: {
+                    System.exit(0);
+                }
+                default: {
+                    System.err.println("Opção inválida, tente novamente.\n");
+                    break;
+                }
+            }
+        } while (true);
     }
 }
--- RBT/src/rbt/Tree.java
+++ RBT/src/rbt/Tree.java
@@ -4,17 +4,44 @@
  */
 package rbt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  *
  * @author Jean.Carli
  */
 public class Tree  {
-
-    public Tree(String file) {
+    
+    private String kv;
+    
+    public void crateTree(String file){
+       try{
+           FileReader arq = new FileReader(file);
+           BufferedReader lerArq = new BufferedReader(arq);
+           
+           String linha = lerArq.readLine();
+           
+           while (linha != null) {
+               System.out.printf("%s\n", linha);
+               linha = lerArq.readLine();
+           }
+           
+           arq.close();
+       } catch(IOException e){
+           System.err.println("Erro ao ler o arquivo: \n" + e.getMessage());
+       }
+    }
+    
+    public void addNodo(String kv){
         
     }
     
-    public void addNodo(){
+    public void rmNodo(String kv){
+        
+    }
+    
+    public void searchNodo(String kv){
         
     }
     
