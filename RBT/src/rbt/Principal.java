5ddbbaae6deb19f221ab6a3c67b4df22d04c4594
JeanDeCarli <jean.carlo.jc1@gmail.com>
2013-10-18 09:55:11 -0300

    Criando projeto RBT

---
+++ RBT/src/rbt/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rbt;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jean.Carli
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("Informe o nome do arquivo com o caminho:\n");
+        
+    }
+}
---
+++ RBT/src/rbt/Tree.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rbt;
+
+/**
+ *
+ * @author Jean.Carli
+ */
+public class Tree  {
+
+    public Tree(String file) {
+        
+    }
+    
+    public void addNodo(){
+        
+    }
+    
+}
