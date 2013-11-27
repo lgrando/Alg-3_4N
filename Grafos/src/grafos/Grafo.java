91a260fb965baafedb9ce4fa480352f8a5b62e6f
JeanDeCarli <jean.carlo.jc1@gmail.com>
2013-11-27 17:15:41 -0200

    concluido sem erro de sintaxe, possivel erro de logica

--- Grafos/src/grafos/Grafo.java
+++ Grafos/src/grafos/Grafo.java
@@ -19,122 +19,155 @@ public class Grafo {
     Scanner scan;
     private ArrayList<Vertice> grafo = new ArrayList<Vertice>();
     private ArrayList<Integer> caminho = new ArrayList<Integer>();
-    int existe = -1;
+    int verticeExiste = -1;
+    int vizinhoExiste = -1;
 
     public Grafo() throws FileNotFoundException {
         this.scan = new Scanner(new FileReader("Grafo.txt"));
     }
 
     public void montarGrafo() throws FileNotFoundException, IOException {
-        String valor;
+        String idVertice, vizinho;
 
-        valor = scan.next();
+        idVertice = scan.next();
 
         Vertice ver = new Vertice();
-        ver.setId(Integer.parseInt(valor));
+        ver.setId(Integer.parseInt(idVertice));
         ver.setEtiqueta(Integer.MAX_VALUE);
-        ver.setVerticeVizinhos(Integer.parseInt(scan.next()));
+        vizinho = scan.next();
+        this.vizinhoExiste = vizinhoExiste(Integer.parseInt(vizinho));
+        if (this.vizinhoExiste == -1) {
+            Vertice ver3 = new Vertice();
+
+            ver3.setId(Integer.parseInt(vizinho));
+            ver3.setEtiqueta(Integer.MAX_VALUE);
+
+            grafo.add(ver3);
+        }
+        ver.setVerticeVizinhos(Integer.parseInt(vizinho));
         ver.setPeso(Integer.parseInt(scan.next()));
 
         grafo.add(ver);
 
         while (scan.hasNext()) {
-            valor = scan.next();
+            idVertice = scan.next();
 
-            this.existe = verticeExiste(Integer.parseInt(valor));
-            if (this.existe != -1) {
-                grafo.get(this.existe).setVerticeVizinhos(Integer.parseInt(scan.next()));
-                grafo.get(this.existe).setPeso(Integer.parseInt(scan.next()));
+            this.verticeExiste = verticeExiste(Integer.parseInt(idVertice));
+            if (this.verticeExiste != -1) {
+                vizinho = scan.next();
+                vizinhoExiste = vizinhoExiste(Integer.parseInt(vizinho));
+                if (vizinhoExiste == -1) {
+                    Vertice ver4 = new Vertice();
+
+                    ver4.setId(Integer.parseInt(vizinho));
+                    ver4.setEtiqueta(Integer.MAX_VALUE);
+
+                    grafo.add(ver4);
+                }
+                grafo.get(this.verticeExiste).setVerticeVizinhos(Integer.parseInt(vizinho));
+                grafo.get(this.verticeExiste).setPeso(Integer.parseInt(scan.next()));
             } else {
                 Vertice ver1 = new Vertice();
-                ver1.setId(Integer.parseInt(valor));
+                ver1.setId(Integer.parseInt(idVertice));
                 ver1.setEtiqueta(Integer.MAX_VALUE);
-                ver1.setVerticeVizinhos(Integer.parseInt(scan.next()));
+                vizinho = scan.next();
+                this.vizinhoExiste = vizinhoExiste(Integer.parseInt(vizinho));
+                if (this.vizinhoExiste == -1) {
+                    Vertice ver5 = new Vertice();
+
+                    ver5.setId(Integer.parseInt(vizinho));
+                    ver.setEtiqueta(Integer.MAX_VALUE);
+
+                    grafo.add(ver5);
+                }
+                ver1.setVerticeVizinhos(Integer.parseInt(vizinho));
                 ver1.setPeso(Integer.parseInt(scan.next()));
                 grafo.add(ver1);
             }
         }
-        exibeGrafo();
     }
 
     public int verticeExiste(int id) {
-        this.existe = -1;
+        this.verticeExiste = -1;
         for (int i = 0; i < grafo.size(); i++) {
             if (grafo.get(i).getId() == id) {
-                this.existe = i;
+                this.verticeExiste = i;
                 break;
             }
         }
-        return this.existe;
-    }
-
-    public void exibeGrafo() {
-        for (int i = 0; i < grafo.size(); i++) {
-            for (int j = 0; j < grafo.get(i).getVerticeVizinhos().size(); j++) {
-                System.out.println(grafo.get(i).getId() + " " + grafo.get(i).getVerticeVizinhos().get(j) + " " + grafo.get(i).getPeso().get(j));
-            }
-        }
+        return this.verticeExiste;
     }
     
-    public void zeraEtiquetaVerticeInicial(int from){
+    public int vizinhoExiste(int id) {
+        this.vizinhoExiste = -1;
+        for (int i = 0; i < grafo.size(); i++) {
+            if (grafo.get(i).getId() == id) {
+                this.vizinhoExiste = i;
+                break;
+            }
+        }
+        return this.vizinhoExiste;
+    }
+
+    public void zeraEtiquetaVerticeInicial(int from) {
         for (int i = 0; i < grafo.size(); i++) {
             if (grafo.get(i).getId() == from) {
                 grafo.get(i).setEtiqueta(0);
-                grafo.get(i).setVisitado(true);
             }
         }
     }
 
     public void Dijkstra() {
         int existeVerticeNaoVisitado;
-        int menorEtiquetaNaoVisitada = 0;
-        do{
+        int menorEtiquetaNaoVisitada = Integer.MAX_VALUE;
+        do {
             existeVerticeNaoVisitado = 0;
-            
-            for(int i = 0; i < grafo.size(); i++){
-                if(grafo.get(i).getEtiqueta() <= menorEtiquetaNaoVisitada && (!grafo.get(i).isVisitado())){
+
+            for (int i = 0; i < grafo.size(); i++) {
+                if (grafo.get(i).getEtiqueta() <= menorEtiquetaNaoVisitada && (grafo.get(i).isVisitado() == false)) {
                     menorEtiquetaNaoVisitada = grafo.get(i).getEtiqueta();
                 }
             }
-            
-            for(int i = 0; i < grafo.size(); i++){
-                if(grafo.get(i).getEtiqueta() == menorEtiquetaNaoVisitada && (!grafo.get(i).isVisitado())){
+
+            for (int i = 0; i < grafo.size(); i++) {
+                if (grafo.get(i).getEtiqueta() == menorEtiquetaNaoVisitada && (grafo.get(i).isVisitado() == false)) {
                     grafo.get(i).setVisitado(true);
-                    
-                    for(int j = 0; j <= grafo.get(i).getVerticeVizinhos().size(); j++){
-                        if((grafo.get(grafo.get(i).getVerticeVizinhos().get(j)).getEtiqueta()) > (grafo.get(i).getEtiqueta() + grafo.get(i).getPeso().get(j))){
+
+                    for (int j = 0; j < grafo.get(i).getVerticeVizinhos().size(); j++) {
+                        if ((grafo.get(grafo.get(i).getVerticeVizinhos().get(j)).getEtiqueta()) > (grafo.get(i).getEtiqueta() + grafo.get(i).getPeso().get(j))) {
                             grafo.get(grafo.get(i).getVerticeVizinhos().get(j)).setEtiqueta(grafo.get(i).getEtiqueta() + grafo.get(i).getPeso().get(j));
                             grafo.get(grafo.get(i).getVerticeVizinhos().get(j)).setPrecedente(grafo.get(i).getId());
                         }
                     }
+                    menorEtiquetaNaoVisitada = Integer.MAX_VALUE;
                 }
             }
-            
-            for(int i = 0; i < grafo.size(); i++){
-                if(!grafo.get(i).isVisitado()){
+
+            for (int i = 0; i < grafo.size(); i++) {
+                if (!grafo.get(i).isVisitado()) {
                     existeVerticeNaoVisitado++;
                 }
             }
-            
-        }while(existeVerticeNaoVisitado != 0);
+
+        } while (existeVerticeNaoVisitado != 0);
     }
-    
+
     public String menorCaminho(int from, int to) {
         String menorCaminho = null;
         this.caminho.add(to);
 
-            for (int i = 0; i <= this.grafo.size(); i++) {
-                if (this.grafo.get(i).getId() == to) {
-                    this.caminho.add(this.grafo.get(i).getPrecedente());
-                    to = this.grafo.get(i).getPrecedente();
-                    i = 0;
-                }
+        for (int i = 0; i < this.grafo.size(); i++) {
+            if (this.grafo.get(i).getId() == to) {
+                this.caminho.add(this.grafo.get(i).getPrecedente());
+                to = this.grafo.get(i).getPrecedente();
+                i = 0;
             }
-            
-            for(int i = 0; i <= this.caminho.size(); i++){
-                menorCaminho = menorCaminho + " --> " + this.caminho.get(i);
-            }
-        
+        }
+
+        for (int i = 0; i < this.caminho.size(); i++) {
+            menorCaminho = menorCaminho + " --> " + this.caminho.get(i);
+        }
+
         return menorCaminho;
     }
 }
